63f9dd077ad91d6d5f12729c4b5f627136ddf53e
Edward <Edward@AN515-57>
2025-04-28 15:07:18 -0500

    Adicion de nuevo programa ejemplo para pc3

---
+++ PC3-S3-CasoEjemplo/PracticaDeCampo-S3-Ejemplo/src/practicadecampo/s3/ejemplo/RegistrarDatos.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practicadecampo.s3.ejemplo;
+
+/**
+ *
+ * @author Edward
+ */
+public class RegistrarDatos {
+    private String nombre;
+    private int edad;
+
+    public RegistrarDatos() {
+    }
+
+    public RegistrarDatos(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
