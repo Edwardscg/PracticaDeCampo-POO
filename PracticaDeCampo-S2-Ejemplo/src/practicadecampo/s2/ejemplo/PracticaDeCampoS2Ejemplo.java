385404649ef47805af027342635cfc8887f88e3d
Edward <Edward@AN515-57>
2025-04-27 19:33:55 -0500

    Agregar ejercicio S2

---
+++ PracticaDeCampo-S2-Ejemplo/src/practicadecampo/s2/ejemplo/PracticaDeCampoS2Ejemplo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicadecampo.s2.ejemplo;
+
+/**
+ *
+ * @author Edward
+ */
+public class PracticaDeCampoS2Ejemplo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Crear un objeto Carro
+        Carro miCarro = new Carro("Toyota", "Corolla", 2020);
+        
+        // Mostrar los datos del carro
+        miCarro.mostrarDatos();
+    }
+    
+}
