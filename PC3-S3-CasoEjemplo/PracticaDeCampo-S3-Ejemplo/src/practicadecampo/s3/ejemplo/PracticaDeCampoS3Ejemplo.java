5cd8ffa6a0724a01d48a190dbea2d76e3003b9fb
Edward <Edward@AN515-57>
2025-04-29 14:46:33 -0500

    Cambios en el programa

--- PC3-S3-CasoEjemplo/PracticaDeCampo-S3-Ejemplo/src/practicadecampo/s3/ejemplo/PracticaDeCampoS3Ejemplo.java
+++ PC3-S3-CasoEjemplo/PracticaDeCampo-S3-Ejemplo/src/practicadecampo/s3/ejemplo/PracticaDeCampoS3Ejemplo.java
@@ -14,7 +14,22 @@ public class PracticaDeCampoS3Ejemplo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Crear un objeto de RegistrarDatos sin parámetros
+        RegistrarDatos persona1 = new RegistrarDatos();
+        // Establecer los valores usando los setters
+        persona1.setNombre("Edward");
+        persona1.setEdad(20);
+        
+        // Mostrar los datos utilizando los getters
+        System.out.println("Nombre: " + persona1.getNombre());
+        System.out.println("Edad: " + persona1.getEdad());
+        
+        // Crear otro objeto con parámetros en el constructor
+        RegistrarDatos persona2 = new RegistrarDatos("Steven", 25);
+        
+        // Mostrar los datos del segundo objeto
+        System.out.println("Nombre: " + persona2.getNombre());
+        System.out.println("Edad: " + persona2.getEdad());
     }
     
 }
